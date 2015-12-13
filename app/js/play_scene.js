f9eb262bb493407e54b89216dcd8a2eea9c46387
Tobias Pickel <tobias.pickel@uni-ulm.de>
2015-12-13 14:43:15 +0100

    added follow camera fixed some code styling

--- app/js/play_scene.js
+++ app/js/play_scene.js
@@ -5,13 +5,23 @@ var Player = require('./player/player.js');
 
 var PlayScene = {
   create: function () {
+
+    // add backgorund logo
     var logo = this.game.add.sprite(
       this.game.world.centerX, this.game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
+
+    // create gameWorld
+    this.game.world.setBounds(0, 0, 1400, 1400);
+
+    // add player to gameWorld
     Player.create(this.game);
+
 
   },
   update: function () {
+
+    // update Player
     Player.update();
   }
 };
--- app/js/player/player.js
+++ app/js/player/player.js
@@ -5,34 +5,38 @@ var Player = {
     console.log('construct');
   },
   create: function (game) {
-    console.log('Kuchen');
+
+    // save the game reference
     this.game = game;
+
+    // create player sprite
     this.sprite = this.game.add.sprite(200, 200, 'mushroom');
+
+    // hook the camera to the player sprite
+    // use FOLLOW_LOCKON as follow style
+    this.game.camera.follow(this.sprite, Phaser.Camera.FOLLOW_LOCKON);
+
+    // create cursors
     this.cursors = game.input.keyboard.createCursorKeys();
   },
   update: function () {
     // console.log(this.game);
     // console.log('update', this.cursors);
-    if (this.cursors.up.isDown)
-    {
+    if (this.cursors.up.isDown) {
       //up
       this.sprite.y -= 10;
-    }
-    else if (this.cursors.down.isDown)
-    {
+
+    } else if (this.cursors.down.isDown) {
       //down
       this.sprite.y += 10;
-    }
 
-    if (this.cursors.left.isDown)
-    {
+    } if (this.cursors.left.isDown) {
       //left
-      this.sprite.x -= 10
-    }
-    else if (this.cursors.right.isDown)
-    {
+      this.sprite.x -= 10;
+
+    } else if (this.cursors.right.isDown) {
       //right
-      this.sprite.x += 10
+      this.sprite.x += 10;
     }
   }
 }
