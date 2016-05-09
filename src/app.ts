dd3065cd88ea41df60e091ae8dfcfbd53a3670f9
Tobias Pickel <tobias.pickel@uni-ulm.de>
2016-05-09 19:51:35 +0200

    init game class

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
+///<reference path="../dist/Excalibur.d.ts" />
+
 declare function require(string): string;
 require('../style/main.less');
 
-import test = require('./test');
-import modal = require('./components/modal/ctrl');
+let game = new ex.Engine({
+	width: 800,
+	height: 600
+});
 
-
-// var navigation = new Template.Template();
-// document.write(test());
-var modals = modal;
+game.start();
